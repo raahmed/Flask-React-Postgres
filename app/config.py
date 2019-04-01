775a9915440b97ac75df6427893f75477e8094c8
Bryan Van de Ven <bryanv@anaconda.com>
2019-04-01 12:54:20 -0700

    Add Keyvault support for db credentials

--- app/config.py
+++ app/config.py
@@ -1,5 +1,25 @@
 import os
 
+from azure.keyvault import KeyVaultClient
+from msrestazure.azure_active_directory import MSIAuthentication
+
+def _get_database_uri():
+
+    if "APPSETTING_WEBSITE_SITE_NAME" in os.environ:
+        credentials = MSIAuthentication(resource='https://vault.azure.net')
+        key_vault_client = KeyVaultClient(credentials)
+
+        key_vault_uri = os.environ.get("KEY_VAULT_URI")
+
+        return key_vault_client.get_secret(
+            key_vault_uri,
+            "database-url",
+            ""
+        )
+    else:
+        return TestingConfig.SQLALCHEMY_DATABASE_URI
+
+
 
 class BaseConfig(object):
     DEBUG = False
@@ -14,7 +34,7 @@ class TestingConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', TestingConfig.SQLALCHEMY_DATABASE_URI)
+    SQLALCHEMY_DATABASE_URI = _get_database_uri(),
     APPINSIGHTS_INSTRUMENTATIONKEY = os.environ.get('APPINSIGHTS_INSTRUMENTATIONKEY', TestingConfig.APPINSIGHTS_INSTRUMENTATIONKEY)
     SECRET_KEY = os.environ.get('SECRET_KEY', TestingConfig.SECRET_KEY)
     
