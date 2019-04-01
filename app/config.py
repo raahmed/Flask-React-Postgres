a467dc74fd6d1c3dd44f60e5aeaa8e6208eb7fdc
Bryan Van de Ven <bryanv@anaconda.com>
2019-04-01 13:51:16 -0700

    address comments

--- app/config.py
+++ app/config.py
@@ -3,22 +3,24 @@ import os
 from azure.keyvault import KeyVaultClient
 from msrestazure.azure_active_directory import MSIAuthentication
 
+def _is_app_service():
+    return 'APPSETTING_WEBSITE_SITE_NAME' in os.environ
+
 def _get_database_uri():
 
-    if "APPSETTING_WEBSITE_SITE_NAME" in os.environ:
-        credentials = MSIAuthentication(resource='https://vault.azure.net')
+    if _is_app_service():
+        credentials = MSIAuthentication()
         key_vault_client = KeyVaultClient(credentials)
 
         key_vault_uri = os.environ.get("KEY_VAULT_URI")
 
         return key_vault_client.get_secret(
-            key_vault_uri,
-            "database-url",
-            ""
+            vault_base_url=key_vault_uri,
+            secret_name="database-url",
+            secret_version="" # empty string -> latest secret version
         )
     else:
         return TestingConfig.SQLALCHEMY_DATABASE_URI
-
 
 
 class BaseConfig(object):
