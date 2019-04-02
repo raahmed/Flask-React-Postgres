3f6edce640b136431410d49838d496dc202532fc
Bryan Van de Ven <bryanv@anaconda.com>
2019-04-02 09:02:55 -0700

    Add logging to debug keyvault issue

--- app/config.py
+++ app/config.py
@@ -1,25 +1,34 @@
+import logging
 import os
 
 from azure.keyvault import KeyVaultClient
 from msrestazure.azure_active_directory import MSIAuthentication
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 def _is_app_service():
     return 'APPSETTING_WEBSITE_SITE_NAME' in os.environ
 
 def _get_database_uri():
-
     if _is_app_service():
+        logger.info("Getting Production DB credentials from Keyvault")
         credentials = MSIAuthentication()
         key_vault_client = KeyVaultClient(credentials)
 
         key_vault_uri = os.environ.get("KEY_VAULT_URI", "PUT YOUR KV BASE URL HERE")
+        logger.info("Using KEY_VAULT_URI: {}".format(key_vault_uri))
 
-        return key_vault_client.get_secret(
+        result = key_vault_client.get_secret(
             vault_base_url=key_vault_uri,
             secret_name="database-url",
             secret_version="" # empty string -> latest version
         )
+        logger.info("Secret info: {} {}".format(type(result), len(result))) # XXX TEMP DEBUG
+        return result
+
     else:
+        logger.info("Using local Testing Database")
         return TestingConfig.SQLALCHEMY_DATABASE_URI
 
 
